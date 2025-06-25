70f54f235c6442a2c30ffda1f1021de5aa212db8
Akshaygit777 <cr380019@gmail.com>
2025-06-25 22:08:50 +0530

    created initial signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,33 @@
+import path from 'path';
+import { createBrowserRouter,RouterProvider} from 'react-router-dom';
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
 import './App.css';
 import Navbar from './components/shared/Navbar';
+import  Home  from './components/Home';
+import { Component } from 'lucide-react';
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  },
+  {
+  path:'/signup',
+  element:<Signup/>
+  }
+
+])
 
 function App() {
-  return ( <>
-  <Navbar/>
-  </>
+  return ( <div>
+
+<RouterProvider router = {appRouter}/>
+  </div>
+  
    
   )
 }
