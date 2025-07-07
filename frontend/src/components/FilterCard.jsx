af75d6f67abb2a3bcbb92405157aa7c06442b9a4
Akshaygit777 <cr380019@gmail.com>
2025-07-07 23:29:09 +0530

    started building filtersection for Jobs section

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+const filterData = [
+  {
+  filterType :"Location",
+  array:["delhi NCR","Banglore","Hyderabad","pune","Mumbai"]
+  },
+  {
+  filterType :"Industry",
+  array:["Frontend developer","Backend developer","Fullstack developer","Data Analyst"]
+  },
+  {
+    filterType :"Salary",
+  array:["0-40k","40k=1lakh","1lakh to 5 lakh"]
+  },
+  
+]
 
 const FilterCard = () => {
   return (
-    <div>FilterCard</div>
+    <div>
+
+      <h1>Filter Jobs</h1>
+
+    </div>
   )
 }
 
