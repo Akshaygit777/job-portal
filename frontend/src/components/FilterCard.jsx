f27727c4611cde355218baa89a70300dbd544f42
Akshaygit777 <cr380019@gmail.com>
2025-07-08 22:43:54 +0530

    built filtercard for Jobs section

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,25 +1,47 @@
 import React from 'react'
+import { RadioGroup, RadioGroupItem } from './ui/radio-group'
+import { Label } from '@radix-ui/react-label'
 const filterData = [
   {
   filterType :"Location",
   array:["delhi NCR","Banglore","Hyderabad","pune","Mumbai"]
   },
   {
-  filterType :"Industry",
+  filterType :"Roles",
   array:["Frontend developer","Backend developer","Fullstack developer","Data Analyst"]
   },
   {
     filterType :"Salary",
-  array:["0-40k","40k=1lakh","1lakh to 5 lakh"]
+  array:["4LPA-8LPA","9LPA-12LPA","12LPA Above"]
   },
   
 ]
 
 const FilterCard = () => {
   return (
-    <div>
+    <div className='w-full bg-white p-3 rounded-md'>
 
-      <h1>Filter Jobs</h1>
+      <h1 className='font-bold text-lg '>Filter Jobs</h1>
+      <hr  className='mt-3'/>
+      <RadioGroup className="text-bold">
+        {
+          filterData.map((data, index)=> (
+            <div>
+              <h1 className='font-bold text-lg'>{data.filterType}</h1>
+              {
+                data.array.map((item, index) => {
+                  return (
+                    <div className='flex items-center space-x-2 my-2'>
+                      <RadioGroupItem value={item}/>
+                      <Label>{item}</Label>
+                    </div>
+                  )
+                })
+}
+            </div>
+        ))
+      }
+      </RadioGroup>
 
     </div>
   )
