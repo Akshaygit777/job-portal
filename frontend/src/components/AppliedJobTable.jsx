a6ad829ec6d6dfc7d495c0efbdaae57b24ebc99f
Akshaygit777 <cr380019@gmail.com>
2025-07-14 21:30:10 +0530

    profile section completed

--- frontend/src/components/AppliedJobTable.jsx
+++ frontend/src/components/AppliedJobTable.jsx
@@ -1,8 +1,36 @@
+import { Badge } from './ui/badge'
+import { Table, TableCaption, TableCell, TableHead, TableHeader, TableRow, TableBody} from './ui/table'
 import React from 'react'
 
 const AppliedJobTable = () => {
   return (
-    <div>AppliedJobTable</div>
+    <div>
+      <Table>
+        <TableCaption>A list of your applied jobs 
+
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Date</TableHead>
+            <TableHead>Job Role</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {
+          [1,2,3,4].map((item,index) => (
+            <TableRow key = {index}>
+              <TableCell>20-06-2025</TableCell>
+              <TableCell>Frontend Developer</TableCell>
+              <TableCell>Google</TableCell>
+              <TableCell className="text-right"><Badge className="bg-black text-white rounded-full">Selected</Badge></TableCell>
+            </TableRow>
+          ))
+          }
+        </TableBody>
+      </Table>
+    </div>
   )
 }
 
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -43,7 +43,7 @@ const Profile = () => {
               <h1 className='font-bold '>Skills</h1>
               <div className='flex items-center gap-1 '>
               {
-                skills.length != 0 ? skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>NA</span>
+                skills.length != 0 ? skills.map((item, index) => <Badge  className=" text-white rounded-full bg-black"key={index}>{item}</Badge>) : <span>NA</span>
               }
               </div>
             
@@ -59,7 +59,7 @@ const Profile = () => {
            
     </div>
     <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
-            <h1>Applied jobs</h1>
+            <h1 className='font-bold text-lg my-5'>Applied jobs</h1>
               {        }
               <AppliedJobTable/>
                 
