91cfed5d5596fb39717020eb1da9a52123c822dc
Akshaygit777 <cr380019@gmail.com>
2025-10-02 00:33:26 +0530

    company table displaying correctly

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -16,6 +16,9 @@ const companySchema = new mongoose.Schema({
     location:{
         type:String
     },
+    logo:{
+        type:String 
+    },
     userId:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"user",
