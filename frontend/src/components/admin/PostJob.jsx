441fd440d236f04830d0af08112d31a8786b405d
Akshaygit777 <cr380019@gmail.com>
2025-10-10 23:51:23 +0530

    building postjob

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import Navbar from "../shared/Navbar";
+import { Description } from "@radix-ui/react-dialog";
 
 const PostJob = () => {
-  return (
-    <div>PostJob</div>
-  )
-}
+    const [input,setInput] = useState({
+        title:"",
+        description:"",
+        requirments:"",
+        salary:"",
+        location:"",
+        jobType:"",
+        experiance:"",
+        position:0,
+        companyId:"",
 
-export default PostJob
+    });
+    const changeEventHandler = (e)=>{
+        setInput({...input,[e.target.name]:e.target.value});
+    };
+  return (
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center w-screen my-5">
+        <div>
+          <Label>Title</Label>
+          <Input
+            type="text"
+            name="title"
+            value={input.title}
+            onChange={changeEventHandler}
+            className="focus-visible:ring-offaset-0 focus-visible:ring-0 my-1"
+          />
+        </div>
+        <div>
+          <Label>Title</Label>
+          <Input
+            type="text"
+            name="title"
+            value={input.title}
+            onChange={changeEventHandler}
+            className="focus-visible:ring-offaset-0 focus-visible:ring-0 my-1"
+          />
+        </div>
+        <div>
+          <Label>Title</Label>
+          <Input
+            type="text"
+            name="title"
+            value={input.title}
+            onChange={changeEventHandler}
+            className="focus-visible:ring-offaset-0 focus-visible:ring-0 my-1"
+          />
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default PostJob;
