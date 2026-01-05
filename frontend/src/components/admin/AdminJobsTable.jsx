674f57d140128496c3bd5edeb8fc57cd93fd35db
Akshaygit777 <cr380019@gmail.com>
2026-01-05 23:56:13 +0530

    building applicants table

--- frontend/src/components/admin/AdminJobsTable.jsx
+++ frontend/src/components/admin/AdminJobsTable.jsx
@@ -42,16 +42,16 @@ const AdminJobsTable = () => {
                                 <TableCell className="text-right cursor-pointer">
                                     <Popover>
                                         <PopoverTrigger><MoreHorizontal /></PopoverTrigger>
-                                        <PopoverContent className="w-32">
+                                        <PopoverContent className="w-32 bg-white">
                                             <div
                                                 onClick={() => navigate(`/admin/companies/${job._id}`)}
-                                                className="flex items-center gap-2 w-fit cursor-pointer"
+                                                className="flex items-center gap-2 w-fit cursor-pointer "
                                             >
                                                 <Edit2 className="w-4" />
                                                 <span>Edit</span>
                                             </div>
-                                            <div className= "flex items-center w-fit gap-2 cursor-pointer">
-                                                <Eye/>
+                                            <div onClick = {() => navigate(`/admin/jobs/${job._id}/applicants`)}className= "flex items-center w-fit gap-2 cursor-pointer mt-2">
+                                                <Eye className='w-4'/>
                                                 <span>Applicants</span>
                                                 </div>
                                         </PopoverContent>
